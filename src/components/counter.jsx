a45198400d9dd0cf472f002aa4d0991084ed2a67
HaoTu1417 <tutunghao@gmail.com>
2021-05-26 14:55:21 +0700

    single truth source

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
     tags: [],
   };
 
@@ -37,9 +37,9 @@ class Counter extends Component {
         </button>
         <button
           className="btn btn-danger btn-sm m-2"
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
         >
-          Delete{" "}
+          Delete
         </button>
       </div>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,18 +10,32 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
-  handleDelete = () => {
-    console.log("deleted called");
+  handleDelete = (counterId) => {
+    console.log("deleted called", counterId);
+    const counters = this.state.counters.filter((c) => c.id != counterId);
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const countes = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ countes });
   };
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            selected={true}
             onDelete={this.handleDelete}
+            counter={counter}
           />
         ))}
       </div>
